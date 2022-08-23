8e681bff70eb96cbc104d2969f127a5d68dd082c
Vanessa Rivera-Quinones <43471944+vriveraq@users.noreply.github.com>
2022-08-23 10:41:59 +0200

    Add search by city

--- app.py
+++ app.py
@@ -7,30 +7,36 @@ from scripts.interactiveMap import get_location_interactive
 def main():
     st.title('Belgian AI Landscape')
     st.write('A list highlighting the companies focused on data science and artificial intelligence in Belgium.')
-    st.image("images/ai_animation.gif")
-    region = st.multiselect(
-     'Regions:',
-     ['Brussels', 'Flanders', 'Wallonia'],
-     ['Brussels', 'Flanders', 'Wallonia'])
-    
+
     filepath = os.path.join(os.path.dirname(__file__), "data/AILandscape_geocoded.csv")
     database = pd.read_csv(filepath)
 
-    if region != '':
-        df = database[database['Region'].isin(region)]
-      
+    region = st.multiselect(
+     'Regions:', options = database['Region'].unique().tolist(), default = ['Flanders'])
+    
+    city = st.multiselect(
+     'City:', options = database['City'].unique().tolist(), default=['Leuven'])
+    
+    if city !=[]:
+        df = database[database['City'].isin(city)]
         figure = get_location_interactive(df)
         st.plotly_chart(figure)
-        st.dataframe(df)
+        st.dataframe(df[['Company Name', 'Link', 'Region', 'Address']])
+    
+    if region != '':
+        df = database[database['Region'].isin(region)]
+        figure = get_location_interactive(df)
+        st.plotly_chart(figure)
+        st.dataframe(df[['Company Name', 'Link', 'Region', 'Address']])
 
-        csv = df.to_csv().encode('utf-8')
-        st.download_button(
-                "Press to Download",
-                csv,
-                "file.csv",
-                "text/csv",
-                key='download-csv'
-                )
+    csv = df.to_csv().encode('utf-8')
+    st.download_button(
+            "Press to Download",
+            csv,
+            "file.csv",
+            "text/csv",
+            key='download-csv'
+            )
 
   
 if __name__ == '__main__':
